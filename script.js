d5e1e484ed0945cf0e692f464113b9c143416cd3
freshia-njoki <fnjokimacharia@gmail.com>
2023-05-14 15:12:24 +0300

    improved edit and delete functionalities

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
+// // Define an array to store the user data
+// var users = [];
+
+// // Function to add a user to the array
+// function addUser() {
+//   // Get the input values
+//   var name = document.getElementById("name").value;
+//   var idNumber = document.getElementById("id-number").value;
+//   var country = document.getElementById("country").value;
+//   var languages = document.getElementById("languages").value;
+
+//   // Add the user data to the array
+//   users.push({
+//     name: name,
+//     idNumber: idNumber,
+//     country: country,
+//     languages: languages,
+//   });
+
+//   // Clear the form
+//   document.getElementById("name").value = "";
+//   document.getElementById("id-number").value = "";
+//   document.getElementById("country").value = "";
+//   document.getElementById("languages").value = "";
+
+//   // Refresh the user list
+//   refreshUserList();
+// }
+
+// // Function to delete a user from the array
+// function deleteUser(index) {
+//   // Remove the user data from the array
+//   users.splice(index, 1);
+
+//   // Refresh the user list
+//   refreshUserList();
+// }
+
+// // Function to edit a user in the array
+// function editUser(index) {
+//   // Get the user data from the array
+//   var user = users[index];
+
+//   // Update the user data with the input values
+//   user.name = document.getElementById("edit-name").value;
+//   user.idNumber = document.getElementById("edit-id-number").value;
+//   user.country = document.getElementById("edit-country").value;
+//   user.languages = document.getElementById("edit-languages").value;
+
+//   // Refresh the user list
+//   refreshUserList();
+
+//   // Hide the edit form
+//   document.getElementById("edit-form").style.display = "none";
+// }
+
+// // Function to display the user data in a table
+// function refreshUserList() {
+//   var userList = document.getElementById("user-list");
+//   userList.innerHTML = "";
+//   for (var i = 0; i < users.length; i++) {
+//     var user = users[i];
+//     var row = document.createElement("tr");
+//     var nameCell = document.createElement("td");
+//     var idNumberCell = document.createElement("td");
+//     var countryCell = document.createElement("td");
+//     var languagesCell = document.createElement("td");
+//     var actionsCell = document.createElement("td");
+//     var editButton = document.createElement("button");
+//     var deleteButton = document.createElement("button");
+//     editButton.innerHTML = "Edit";
+//     editButton.onclick = (function (index) {
+//       return function () {
+//         showEditForm(index);
+//       };
+//     })(i);
+//     deleteButton.innerHTML = "Delete";
+//     deleteButton.onclick = (function (index) {
+//       return function () {
+//         deleteUser(index);
+//       };
+//     })(i);
+//     nameCell.innerHTML = user.name;
+//     idNumberCell.innerHTML = user.idNumber;
+//     countryCell.innerHTML = user.country;
+//     languagesCell.innerHTML = user.languages;
+//     actionsCell.appendChild(editButton);
+//     actionsCell.appendChild(deleteButton);
+//     row.appendChild(nameCell);
+//     row.appendChild(idNumberCell);
+//     row.appendChild(countryCell);
+//     row.appendChild(languagesCell);
+//     row.appendChild(actionsCell);
+//     userList.appendChild(row);
+//   }
+// }
+
+// // Function to show the edit form
+// function showEditForm(index) {
+//   // Get the user data from the array
+//   var user = users[index];
+
+//   // Set the input values in the edit form
+//   document.getElementById("edit-name").value = user.name;
+//   document.getElementById("edit-id-number").value = user.idNumber;
+//   document.getElementById("edit-country").value = user.country;
+//   document.getElementById("edit-languages").value;
+// }
+
 // Initialize global variables
 let editingRow = null;
 
